80dfef482e2d595eb7f5fdf96bf83362114fd442
Aradhya <aradhya@my.yorku.ca>
2024-01-05 03:48:58 -0500

    WIP-2

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,27 +1,24 @@
 import React from "react";
+import './Experience.scss';
 
 const Experience = () => {
   return (
-    <div className="row g-0">
-      <div className="col-md-4">
-        <div className="card bg-light">
-          <div className="card-body">
-            {/* Sidebar content here */}
-            <p className="card-text">Sidebar Content</p>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-8">
-        <div className="card bg-light mb-3" style={{ maxWidth: "18rem" }}>
-          <div className="card-header">Header</div>
-          <div className="card-body">
-            <h5 className="card-title">Light card title</h5>
-            <p className="card-text">
+    <div className="container-card">
+      <div className="full-card">
+        <div className="sidebar">January 2023-Present</div>
+        <div
+          class="card text-white bg-transparent mb-3"
+          style={{ maxWidth: "40rem" }}
+        >
+          <div class="card-header">Intermediate Business Analyst @ CIBC</div>
+          <div class="card-body">
+            <h5 class="card-title">Light card title</h5>
+            <p class="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </p>
+            <footer class="card-footer">Hello</footer>
           </div>
-          <div className="card-footer">Footer</div>
         </div>
       </div>
     </div>
